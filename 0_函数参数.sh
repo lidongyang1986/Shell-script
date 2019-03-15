fd8d1d91bd7bced9cdc5692655fa195b40fa715d
lidongyang1986 <lidongyang@ele.uri.edu>
2019-03-14 21:27:38 -0400

    Update 0_函数参数.sh

--- 0_函数参数.sh
+++ 0_函数参数.sh
@@ -2,18 +2,53 @@
 
 #!/bin/bash
 
+
+function menu() 
+
+{    
+
+	clear    
+
+	echo    
+
+	echo -e "\t\t\tTest Options Menu\n"
+
+	echo -e "\t1: 128k_overall_randwrite"
+
+	read -n 3 option
+
+}
+
+
 function identify_disk()
 
 {
     echo $1
 	echo $2	
 	
-	ldy="lidongyang Hynix Memory"
+	ldy=$(echo "lidongyang Hynix Memory")   #函数的函数
 	echo  $ldy
+	
+	#echo -e "\033[1;31mWarnning:the device under test $1 is absent ...\033[0m"
 }
+
+
+c=1
+while [ $c -le 5 ]
+do
+	echo "Welcone $c times"
+	(( c++ ))
+done
+
 
 dev_var="/dev/nvme0n2"
 abc=2
 identify_disk $dev_var $abc
 
 echo "Hello World!"
+
+
+
+
+
+
